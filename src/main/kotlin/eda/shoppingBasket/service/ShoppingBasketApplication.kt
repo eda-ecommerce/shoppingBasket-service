b5a29d54bd1a2fc16fbc1b76092d2e0341fdf6eb
JannikAlx <jannik-alx@gmx.de>
2024-02-06 16:27:44 +0100

    fix: forgot application class

---
+++ src/main/kotlin/eda/shoppingBasket/service/ShoppingBasketApplication.kt
@@ -0,0 +1,11 @@
+package eda.shoppingBasket.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShoppingBasketApplication {
+}
+fun main(args: Array<String>) {
+    runApplication<ShoppingBasketApplication>(*args)
+}
