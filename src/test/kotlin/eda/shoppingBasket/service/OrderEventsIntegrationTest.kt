35b4a25cfb2c0fe0b7a0366dcd15d60bede8c843
JannikAlx <jannik-alx@gmx.de>
2024-02-20 02:10:08 +0100

    chore: split event test into offering / shoppingBasket

---
+++ src/test/kotlin/eda/shoppingBasket/service/OrderEventsIntegrationTest.kt
@@ -0,0 +1,114 @@
+package eda.shoppingBasket.service
+
+import eda.shoppingBasket.service.application.OfferingService
+import eda.shoppingBasket.service.application.ShoppingBasketService
+import eda.shoppingBasket.service.eventing.OfferingConsumer
+import eda.shoppingBasket.service.model.dto.ShoppingBasketDTO
+import eda.shoppingBasket.service.model.entity.ItemState
+import eda.shoppingBasket.service.model.entity.Offering
+import eda.shoppingBasket.service.repository.OfferingRepository
+import eda.shoppingBasket.service.repository.ShoppingBasketItemRepository
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.test.context.EmbeddedKafka
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+@SpringBootTest
+@ExtendWith(SpringExtension::class)
+@DirtiesContext
+@EmbeddedKafka(partitions = 1, brokerProperties = ["listeners=PLAINTEXT://localhost:9092", "port=9092"])
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("dev")
+class OrderEventsIntegrationTest {
+
+    @Autowired
+    private lateinit var offeringConsumer: OfferingConsumer
+
+    @Autowired
+    private lateinit var offeringRepository: OfferingRepository
+
+    @Autowired
+    private lateinit var shoppingBasketItemRepository: ShoppingBasketItemRepository
+
+    @Autowired
+    private lateinit var shoppingBasketService: ShoppingBasketService
+
+    @Autowired
+    private lateinit var producer: KafkaTemplate<String, String>
+
+    @BeforeEach
+    fun setup(){
+        offeringConsumer.resetLatch()
+    }
+    @Test
+    fun testOfferingUnavailableEvent_UpdatesItemState(){
+        offeringRepository.save(Offering(
+            offeringID = UUID.fromString("ed672bdf-831c-437f-ad41-d89ac2f398a4"),
+            price = 10.0f,
+            quantity = 5
+        ))
+        val sb = shoppingBasketService.createShoppingBasket(ShoppingBasketDTO(
+            null,null
+        ))
+        val updatedSb = shoppingBasketService.addOfferingToShoppingBasket(sb.shoppingBasketId!!, UUID.fromString("ed672bdf-831c-437f-ad41-d89ac2f398a4"), 1)
+        Assertions.assertEquals(1, updatedSb!!.items.size)
+        val items = shoppingBasketItemRepository.findAllByOfferingID(UUID.fromString("ed672bdf-831c-437f-ad41-d89ac2f398a4"))
+        Assertions.assertEquals(1, items.size)
+        val eventString = "{\n" +
+                "    \"id\": \"ed672bdf-831c-437f-ad41-d89ac2f398a4\",\n" +
+                "    \"status\": \"active\",\n" +
+                "    \"quantity\": 5,\n" +
+                "    \"price\": 10,\n" +
+                "    \"product\": {\n" +
+                "        \"id\": \"5de3949d-b496-4aa8-a99b-3d806b8e347f\",\n" +
+                "        \"status\": \"retired\"\n" +
+                "    }\n" +
+                "}"
+        val retireOfferingRecord = ProducerRecord<String, String>("offering", eventString).apply {
+            headers().add("operation", "delete".toByteArray())
+            headers().add("source", "offering-service".toByteArray())
+            headers().add("timestamp", System.currentTimeMillis().toString().toByteArray())
+        }
+        producer.send(retireOfferingRecord).join()
+        val consumed = offeringConsumer.countDownLatch.await(3, TimeUnit.SECONDS)
+        assert(consumed)
+        val updatedItems = shoppingBasketItemRepository.findAllByOfferingID(UUID.fromString("ed672bdf-831c-437f-ad41-d89ac2f398a4"))
+        Assertions.assertTrue(updatedItems.isNotEmpty())
+        Assertions.assertEquals(ItemState.UNAVAILABLE,updatedItems.first().state)
+    }
+    @Test
+    fun consumeOfferingMessage_savesCorrectOffering(){
+        val eventString = "{\n" +
+                "    \"id\": \"ed672bdf-831c-437f-ad41-d89ac2f398a4\",\n" +
+                "    \"status\": \"active\",\n" +
+                "    \"quantity\": 5,\n" +
+                "    \"price\": 10,\n" +
+                "    \"product\": {\n" +
+                "        \"id\": \"5de3949d-b496-4aa8-a99b-3d806b8e347f\",\n" +
+                "        \"status\": \"active\"\n" +
+                "    }\n" +
+                "}"
+
+        val createOfferingRecord = ProducerRecord<String, String>("offering", eventString).apply {
+            headers().add("operation", "create".toByteArray())
+            headers().add("source", "offering-service".toByteArray())
+            headers().add("timestamp", System.currentTimeMillis().toString().toByteArray())
+        }
+        producer.send(createOfferingRecord).join()
+        val consumed = offeringConsumer.countDownLatch.await(10, TimeUnit.SECONDS)
+        assert(consumed)
+        val savedOffering = offeringRepository.findById(UUID.fromString("ed672bdf-831c-437f-ad41-d89ac2f398a4"))
+        Assertions.assertEquals(UUID.fromString("ed672bdf-831c-437f-ad41-d89ac2f398a4"), savedOffering.get().offeringID)
+    }
+}
--- src/test/kotlin/eda/shoppingBasket/service/ShoppingBasketEventIntegrationTest.kt
+++ src/test/kotlin/eda/shoppingBasket/service/ShoppingBasketEventIntegrationTest.kt
@@ -1,24 +1,12 @@
 package eda.shoppingBasket.service
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.google.gson.Gson
-import com.ninjasquad.springmockk.MockkBean
-import eda.shoppingBasket.service.application.OfferingService
-import eda.shoppingBasket.service.application.ShoppingBasketService
-import eda.shoppingBasket.service.eventing.OfferingConsumer
-import eda.shoppingBasket.service.eventing.OfferingEvent
 import eda.shoppingBasket.service.eventing.SBOperation
 import eda.shoppingBasket.service.eventing.ShoppingBasketProducer
-import eda.shoppingBasket.service.model.OfferingMapper
-import eda.shoppingBasket.service.model.dto.OfferingDTO
 import eda.shoppingBasket.service.model.dto.ShoppingBasketDTO
 import eda.shoppingBasket.service.repository.OfferingRepository
-import io.mockk.every
-import io.mockk.verify
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.header.Header
-import org.apache.kafka.common.header.Headers
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -27,13 +15,10 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.kafka.support.KafkaHeaders
 import org.springframework.kafka.test.context.EmbeddedKafka
-import org.springframework.messaging.Message
-import org.springframework.messaging.support.MessageBuilder
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import java.lang.Thread.sleep
 import java.util.*
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
@@ -43,10 +28,12 @@ import java.util.concurrent.TimeUnit
 @EmbeddedKafka(partitions = 1, brokerProperties = ["listeners=PLAINTEXT://localhost:9092", "port=9092"])
 @ExtendWith(SpringExtension::class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class KafkaTests {
-
-    @Autowired
-    private lateinit var offeringConsumer: OfferingConsumer
+class ShoppingBasketEventIntegrationTest {
+    companion object{
+        private var sbCountDownLatch = CountDownLatch(1)
+        private var message: ShoppingBasketDTO? = null
+        private var headers: Array<Header>? = null
+    }
 
     @Autowired
     lateinit var offeringRepository: OfferingRepository
@@ -54,25 +41,16 @@ class KafkaTests {
     final val logger = org.slf4j.LoggerFactory.getLogger(this.javaClass)
 
     @Autowired
-    lateinit var offeringService: OfferingService
-
-    @Autowired
-    lateinit var template: KafkaTemplate<String, String>
-
-    @Autowired
     private lateinit var producer: ShoppingBasketProducer
 
     @BeforeEach
     fun setup(){
+        logger.info("RESET LATCH")
+        message = null
+        headers = null
         sbCountDownLatch = CountDownLatch(1)
-        offeringConsumer.resetLatch()
         offeringRepository.deleteAll()
-        //every { offeringService.saveOffering(any()) } returns Unit
     }
-
-    var sbCountDownLatch = CountDownLatch(1)
-    var message: ShoppingBasketDTO? = null
-    var headers: Array<Header>? = null
 
     @KafkaListener(topics = ["shopping-basket"])
     fun consumeInMessageFormat(pMessage: ConsumerRecord<String,String>){
@@ -80,29 +58,32 @@ class KafkaTests {
         logger.info("Payload: ${pMessage.value()}")
         val dto = Gson().fromJson(pMessage.value(), ShoppingBasketDTO::class.java)
         pMessage.headers().forEach {
-            logger.info("Header: ${it.key()} : ${it.value()}")
+            logger.info("Header: ${it.key()} : ${String(it.value())}")
         }
         headers = pMessage.headers().toArray()
-        sbCountDownLatch.countDown()
         message = dto
+        sbCountDownLatch.countDown()
     }
 
     @Test
     fun testEmissionCreated(){
+        sleep(500) //prevent double consumption of events
         val testDto = ShoppingBasketDTO(
             shoppingBasketId = UUID.randomUUID(),
             customerId = UUID.randomUUID(),
             totalPrice = 0.0f
         )
         producer.sendMessage(testDto, SBOperation.CREATED)
-        val consumed = sbCountDownLatch.await(10, TimeUnit.SECONDS)
+        val consumed = sbCountDownLatch!!.await(3, TimeUnit.SECONDS)
         assert(consumed)
         assert(message!=null && headers != null)
-        Assertions.assertEquals("${SBOperation.CREATED}", String(headers!!.find { it.key().equals("operation") }!!.value()))
         Assertions.assertEquals(testDto, message)
+        Assertions.assertEquals("${SBOperation.CREATED}", String(headers!!.find { it.key().equals("operation") }!!.value()))
+
     }
     @Test
     fun testEmissionUpdated(){
+        sleep(500) //prevent double consumption of events
         val testDto = ShoppingBasketDTO(
             shoppingBasketId = UUID.randomUUID(),
             customerId = UUID.randomUUID(),
@@ -111,35 +92,10 @@ class KafkaTests {
             )
         )
         producer.sendMessage(testDto, SBOperation.UPDATED)
-        val consumed = sbCountDownLatch.await(10, TimeUnit.SECONDS)
+        val consumed = sbCountDownLatch.await(3, TimeUnit.SECONDS)
         assert(consumed)
         assert(message!=null && headers != null)
         Assertions.assertEquals("${SBOperation.UPDATED}", String(headers!!.find { it.key().equals("operation") }!!.value()))
         Assertions.assertEquals(testDto, message)
-    }
-
-    @Test
-    fun consumeOfferingMessage(){
-        val eventString = "{\n" +
-                "    \"id\": \"ed672bdf-831c-437f-ad41-d89ac2f398a4\",\n" +
-                "    \"status\": \"active\",\n" +
-                "    \"quantity\": 5,\n" +
-                "    \"price\": 10,\n" +
-                "    \"product\": {\n" +
-                "        \"id\": \"5de3949d-b496-4aa8-a99b-3d806b8e347f\",\n" +
-                "        \"status\": \"active\"\n" +
-                "    }\n" +
-                "}"
-
-        val testRecord = ProducerRecord<String, String>("offering", eventString).apply {
-            headers().add("operation", "create".toByteArray())
-            headers().add("source", "offering-service".toByteArray())
-            headers().add("timestamp", System.currentTimeMillis().toString().toByteArray())
-        }
-        template.send(testRecord).join()
-        val consumed = offeringConsumer.countDownLatch.await(10, TimeUnit.SECONDS)
-        assert(consumed)
-        val savedOffering = offeringRepository.findById(UUID.fromString("ed672bdf-831c-437f-ad41-d89ac2f398a4"))
-        Assertions.assertEquals(UUID.fromString("ed672bdf-831c-437f-ad41-d89ac2f398a4"), savedOffering.get().offeringID)
     }
 }
