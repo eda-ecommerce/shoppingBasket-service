5891ca46e903cad4d8fa34e03074d202df0136c4
JannikAlx <jannik-alx@gmx.de>
2024-02-20 02:13:42 +0100

    refactor: fix Header logging, remove unused listener method

--- src/main/kotlin/eda/shoppingBasket/service/eventing/OfferingConsumer.kt
+++ src/main/kotlin/eda/shoppingBasket/service/eventing/OfferingConsumer.kt
@@ -7,7 +7,6 @@ import org.apache.kafka.common.header.Header
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.messaging.Message
 import org.springframework.stereotype.Component
 import java.util.concurrent.CountDownLatch
 
@@ -17,58 +16,39 @@ class OfferingConsumer {
     @Autowired
     private lateinit var offeringService: OfferingService
 
-    private val logger= LoggerFactory.getLogger(this.javaClass)
+    private val logger = LoggerFactory.getLogger(this.javaClass)
 
     var countDownLatch = CountDownLatch(1)
 
-    //@KafkaListener(topics = ["offering"])
-    fun offeringListener(message: Message<OfferingEvent>){
-        logger.info("Offering message received: $message")
-        if (message.headers.containsKey("operation")){
-            val operation = message.headers["operation"]?.toString()
-            when(operation){
-                "delete" -> {
-                    offeringService.deleteOffering(message.payload.id)
-                    countDownLatch.countDown()
-                    return
-                }
-                else -> {
-                    offeringService.saveOffering(message.payload)
-                    countDownLatch.countDown()
-                    return
-                }
-            }
-        }
-    }
     @KafkaListener(topics = ["offering"])
-    fun offeringJsonListener(message: ConsumerRecord<Any, Any>){
+    fun offeringJsonListener(message: ConsumerRecord<Any, Any>) {
         logger.info("Offering message received: ${message.value()}")
-        val payload =  message.value().toString()
+        val payload = message.value().toString()
         val event = Gson().fromJson(payload, OfferingEvent::class.java)
         val headers = message.headers()
         headers.forEach { header: Header ->
-            logger.info("Header: ${header.key()} : ${header.value()}")
-            when (header.key()) {
-                "operation" -> {
-                    val operation = header.value().toString()
-                    when(operation){
-                        "delete" -> {
-                            offeringService.deleteOffering(event.id)
-                            countDownLatch.countDown()
-                            return
-                        }
-                        else -> {
-                            offeringService.saveOffering(event)
-                            countDownLatch.countDown()
-                            return
-                        }
+            logger.info("Header: ${header.key()} : ${String(header.value())}")
+            if (header.key() == "operation") {
+                val operation = String(header.value())
+                when (operation) {
+                    "delete" -> {
+                        offeringService.disableOffering(event.id)
+                        countDownLatch.countDown()
+                        return
                     }
+                    else -> {
+                        offeringService.saveOffering(event)
+                        countDownLatch.countDown()
+                        return
+                    }
+                    //Future: call reEnableOffering(event)
                 }
             }
         }
+        countDownLatch.countDown()
     }
 
-    fun resetLatch(){
+    fun resetLatch() {
         countDownLatch = CountDownLatch(1)
     }
 
