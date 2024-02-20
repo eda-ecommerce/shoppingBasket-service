5513767fbee5edd238c16ec6463d4e325044f3da
JannikAlx <jannik-alx@gmx.de>
2024-02-20 02:11:24 +0100

    chore: cleanup unused file

--- src/main/kotlin/eda/shoppingBasket/service/eventing/OfferingListener.kt
+++
@@ -1,9 +0,0 @@
-package eda.shoppingBasket.service.eventing
-
-import eda.shoppingBasket.service.application.OfferingService
-import org.springframework.stereotype.Component
-
-@Component
-class OfferingListener {
-    lateinit var offeringService: OfferingService
-}
