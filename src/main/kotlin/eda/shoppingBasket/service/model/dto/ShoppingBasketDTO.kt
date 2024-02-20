7aab4a9f2f20e07ba25d6148916caec3031eed1b
JannikAlx <jannik-alx@gmx.de>
2024-02-20 02:09:38 +0100

    chore: add override for equal method for testing

--- src/main/kotlin/eda/shoppingBasket/service/model/dto/ShoppingBasketDTO.kt
+++ src/main/kotlin/eda/shoppingBasket/service/model/dto/ShoppingBasketDTO.kt
@@ -7,6 +7,29 @@ data class ShoppingBasketDTO( // ShoppingBasketAnswerDTO
     var customerId: UUID?,
     var totalPrice: Float = 0.0f,
     var totalItemQuantity: Int = 0,
-    var items: List<ShoppingBasketItemDTO> = mutableListOf(),
+    var items: List<ShoppingBasketItemDTO> = mutableListOf()
 ) {
+    override fun hashCode(): Int {
+        var result = shoppingBasketId?.hashCode() ?: 0
+        result = 31 * result + (customerId?.hashCode() ?: 0)
+        result = 31 * result + totalPrice.hashCode()
+        result = 31 * result + totalItemQuantity
+        result = 31 * result + items.hashCode()
+        return result
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ShoppingBasketDTO
+
+        if (shoppingBasketId != other.shoppingBasketId) return false
+        if (customerId != other.customerId) return false
+        if (totalPrice != other.totalPrice) return false
+        if (totalItemQuantity != other.totalItemQuantity) return false
+        if (items != other.items) return false
+
+        return true
+    }
 }
